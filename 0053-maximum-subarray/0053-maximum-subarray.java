22dc3aac04192b12707c68746a34503988cc20cc
sky5700 <45534292+sky5700@users.noreply.github.com>
2023-10-06 14:19:56 +0530

    Time: 1 ms (100%), Space: 57.8 MB (78.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -11,6 +11,7 @@ class Solution {
         int ultimateMax =nums[0];
         for(int i = 0; i< n; i++)
         {
+            
            if(maxSum <0)
            maxSum = 0; 
           
@@ -21,3 +22,28 @@ class Solution {
         
     }
 }
+ /*public int maxSubArray(int[] nums) {
+
+        int n = nums.length;
+        if (n == 0)
+        return 0;
+        if( n == 1)
+        return nums[0];
+
+        int maxSum  = nums[0];
+        for(int i = 0; i< n; i++)
+        {
+            int sum = nums[i];
+            int max = nums[i];
+            for(int j = i+1; j < n; j++)
+            {
+                sum = sum + nums[j];
+                 max = Math.max(max, sum);
+            }
+
+             maxSum = Math.max(maxSum, max);
+             sum = 0; 
+        }
+        return maxSum;
+        
+    }*/
