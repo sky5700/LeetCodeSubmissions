0a07a0c0ef9f435148365d0b8cbd8c7a35805851
sky5700 <45534292+sky5700@users.noreply.github.com>
2023-10-06 15:41:04 +0530

    Time: 4 ms (66.33%), Space: 43.3 MB (95.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,8 +4,30 @@ class Solution {
 
     if(n == 1 && nums[0] == target)
     return new int[]{0};
+     
+    Map<Integer, Integer > mp = new HashMap<>();
+    //Adding in map with element  as key and index as value
+    for(int i = 0; i<n; i++)
+    {
+        mp.put(nums[i], i);
+    }
 
-    int[] arr = new int[2];
+    for(int i = 0 ; i< n; i++)
+    {
+      int mytrgt =   target - nums[i];
+
+      if(mp.containsKey(mytrgt) && mp.get(mytrgt)!= i)
+       {
+           return new int[]{i,mp.get(mytrgt)};
+           
+       }
+    }
+    return new int[]{};
+   
+    }
+}
+/*
+ int[] arr = new int[2];
      outer: for(int i = 0 ; i< n; i++)
      {
          int diff = target - nums[i];
@@ -20,5 +42,4 @@ class Solution {
          }
      }
      return arr;   
-    }
-}
+*/
