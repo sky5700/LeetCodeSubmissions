0f7242d046ff666147d5aec26854b5bee93272d7
sky5700 <45534292+sky5700@users.noreply.github.com>
2023-10-08 01:47:16 +0530

    Time: 916 ms (5.3%), Space: 47.4 MB (6.98%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -32,3 +32,36 @@ public class Solution {
         
     }
 }
+
+/*
+Time : o(m+n) space o(1)
+Concept:
+Think about the cross connection: 
+Suppose here In Example 1 , a is running 4 times for headA and after poiniting   null , It is now pointing from headB  .
+At thesame time b is now pointing 5 means not yet finiesed. 
+After this Again both will strt iterating and intersect in 8.
+
+If there is no intersection means at a same time after cross connection both will reach to null together. means breaking the while  loop.
+
+Trick used: a 5 times then 3 times , b 3 times then 5 times 
+           total iteration for a  = 8 , b =8. 
+
+           
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //boundary check
+    if(headA == null || headB == null) return null;
+    
+    ListNode a = headA;
+    ListNode b = headB;
+    
+    //if a & b have different len, then we will stop the loop after second iteration
+    while( a != b){
+
+//for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        a = a == null? headB : a.next;
+        b = b == null? headA : b.next;    
+    }
+    
+    return a;
+}
+*/
