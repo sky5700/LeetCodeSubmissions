e4d41ff9519fa511304b928544ea8ec95cb1a85d
sky5700 <45534292+sky5700@users.noreply.github.com>
2023-10-08 03:17:59 +0530

    Time: 14 ms (12.05%), Space: 56 MB (84.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ // o(n), o(n) both
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
@@ -36,3 +37,64 @@ class Solution {
         return true;
     }
 }
+
+/* Here Space o(1)
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode firstHalfEnd = findMiddleAndReverse(head);
+        ListNode secondHalfStart = firstHalfEnd.next;
+
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                // Restore the original linked list state if needed
+                firstHalfEnd.next = reverseList(secondHalfStart);
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the original linked list state
+        firstHalfEnd.next = reverseList(secondHalfStart);
+
+        return true;
+    }
+
+    private ListNode findMiddleAndReverse(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        return prev;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
+*/
